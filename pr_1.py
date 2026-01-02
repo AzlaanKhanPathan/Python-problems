08fc32da253420d766b102d4563d2800a53f6e10
Azlaan Khan <akpglb@gmail.com>
2026-01-02 21:45:30 +0530

    first problem

---
+++ pr_1.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+# Write a python program to swap two variables
+
+a = 10 
+b = 20 
+a,b = b,a
+
+print(a)
+print(b)
+=======
+# Write a python program to swap two variables
+
+a = 10 
+b = 20 
+a,b = b,a
+
+print(a)
+print(b)
+>>>>>>> db9bfa3746b0ca5659f1dee453b60d96c8272cb2
+print(a,b)
