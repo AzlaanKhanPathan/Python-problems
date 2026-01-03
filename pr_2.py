dae242c398e5c44e9185417e169686cde34eeab0
Azlaan Khan <akpglb@gmail.com>
2026-01-03 22:09:47 +0530

    second problem

---
+++ pr_2.py
@@ -0,0 +1,21 @@
+#  Take user input and display it back to the user. 
+
+a = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+print(a)
+print(age)
+
+# You can add this to improve the program
+# if age >= 18:
+#     print("Welcome to the party", a)
+# else: 
+#     print("You are not eligible", a)
+
+
+
+
+
+
+
+
+
