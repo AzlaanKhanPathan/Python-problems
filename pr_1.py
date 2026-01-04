f6f72c927544ead457947620470404fdadcc3642
Azlaan Khan <akpglb@gmail.com>
2026-01-04 22:03:35 +0530

    first problem

--- pr_1.py
+++ pr_1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # Write a python program to swap two variables
 
 a = 10 
@@ -7,7 +7,7 @@ a,b = b,a
 
 print(a)
 print(b)
-=======
+
 # Write a python program to swap two variables
 
 a = 10 
@@ -16,5 +16,5 @@ a,b = b,a
 
 print(a)
 print(b)
->>>>>>> db9bfa3746b0ca5659f1dee453b60d96c8272cb2
+
 print(a,b)
