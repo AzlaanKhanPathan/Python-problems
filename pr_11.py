466e06879451b462febeff56fc78b3fc2d934b81
Azlaan Khan <akpglb@gmail.com>
2026-01-19 21:25:00 +0530

    eleventh problem

---
+++ pr_11.py
@@ -0,0 +1,7 @@
+# Write a program to find the sum of first N natural numbers.
+
+def first_num():
+    arr = [1,2,3,4,5,6,7,8,9,10]
+    return f"The sum of first n natural numbers is {sum(arr)}"
+
+print(first_num())
